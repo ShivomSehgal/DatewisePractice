f5a7c92ef3552d5786b86239099cadaf7d532de2
Billionare <70501837+ShivomSehgal@users.noreply.github.com>
2021-02-04 17:05:24 +0530

    Add files via upload

---
+++ BalancedParanthesis.java
@@ -0,0 +1,32 @@
+import java .util.*;
+public class BalancedParanthesis {
+    public static void main(String[] args) {
+        String str="[[]]";
+        System.out.println(checkh(str));
+    }
+
+    public static boolean checkh(String str){
+       Stack <Character> st=new Stack();
+       char[] arr=str.toCharArray();
+       if(arr.length%2!=0){
+           return false;
+       }
+       char[] arr1=new char[arr.length];
+       for(int i=0;i<arr.length;i++){
+           st.push(arr[i]);
+       } 
+       for(int i=0;i<arr.length;i++){
+           arr1[i]=st.pop();
+       }
+       for(int i=0;i<arr.length;i++){
+       if(arr[i]=='{' && arr1[i]=='}'){
+           return true;
+       }else if(arr[i]=='['&& arr1[i]==']'){
+           return true;
+       }else if(arr[i]=='('&& arr1[i]==')'){
+           return true;
+       }
+    }
+       return false;
+    }
+}
